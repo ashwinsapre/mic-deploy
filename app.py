27aa4e355e939be87d56d281486e93388ef1dda1
ashwinsapre <ashwinsapre@gmail.com>
2021-05-20 14:00:03 +0530

    added gdown

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import io
 import json
 import torch.nn as nn 
 import torchvision.transforms as transforms
+import gdown
 from PIL import Image
 from flask import Flask, jsonify, request, render_template
 from caption import *
@@ -9,16 +10,22 @@ from caption import *
 app = Flask(__name__)
 
 device = "cpu"
-#checkpoint = torch.load('BEST_checkpoint_IU-XRAY.pth.tar', map_location=str(device))
-#decoder = checkpoint['decoder']
-#decoder = decoder.to(device)
-#decoder.eval()
-#encoder = checkpoint['encoder']
-#encoder = encoder.to(device)
-#encoder.eval()
-#with open('wordmap.json', 'r') as j:
-#    word_map = json.load(j)
-#rev_word_map = {v: k for k, v in word_map.items()}
+url="https://drive.google.com/file/d/1---vD9czhFbkX2fjBdL4mpMevRRkThR_/view?usp=sharing"
+output="model.pth.tar"
+gdown.download(url, output, quiet=False)
+md5 = 'fa837a88f0c40c513d975104edf3da17'
+gdown.cached_download(url, output, md5=md5, postprocess=gdown.extractall)
+
+checkpoint = torch.load('model.pth.tar', map_location=str(device))
+decoder = checkpoint['decoder']
+decoder = decoder.to(device)
+decoder.eval()
+encoder = checkpoint['encoder']
+encoder = encoder.to(device)
+encoder.eval()
+with open('wordmap.json', 'r') as j:
+    word_map = json.load(j)
+rev_word_map = {v: k for k, v in word_map.items()}
 
 @app.route('/', methods=['POST'])
 def predict():
