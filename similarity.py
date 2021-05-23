2b402a14cb3a0d1cef1746efdeddf24477456a5f
ashwinsapre <ashwinsapre@gmail.com>
2021-05-23 17:42:34 +0530

    fixed indents

--- similarity.py
+++ similarity.py
@@ -8,12 +8,10 @@ def similarity_check(sample_path):
 	gdown.download(url, output, quiet=False)
 	md5 = '251e16d46507539f68b64dc084500eda'
 	gdown.cached_download(url, output, md5=md5)
-
 	mean = np.load('mean.npy')
-    sample = Image.open(sample_path).convert('RGB')
-    w = min(sample.size[0], sample.size[1])
-    sample = sample.resize((w, w))
-    sample = np.array(sample).reshape(1, -1)
-    mean = np.resize(mean,(w, w, 3)).reshape(1, -1)
-
-    return cosine_similarity(mean, sample)
+	sample = Image.open(sample_path).convert('RGB')
+	w = min(sample.size[0], sample.size[1])
+	sample = sample.resize((w, w))
+	sample = np.array(sample).reshape(1, -1)
+	mean = np.resize(mean,(w, w, 3)).reshape(1, -1)
+	return cosine_similarity(mean, sample)
