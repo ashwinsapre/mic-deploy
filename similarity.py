e193e701bd571478c308bc319125266a88ae73d1
ashwinsapre <ashwinsapre@gmail.com>
2021-05-23 17:28:17 +0530

    added mean.npy

--- similarity.py
+++ similarity.py
@@ -5,6 +5,12 @@ import cv2
 
 
 def similarity_check(sample_path):
+	url="https://drive.google.com/uc?id=1IhwcaL-JKxb4kCl_Lr151fjpWuIHn0Mv&export=download"
+	output="mean.npy"
+	gdown.download(url, output, quiet=False)
+	md5 = '251e16d46507539f68b64dc084500eda'
+	gdown.cached_download(url, output, md5=md5)
+	
     mean = np.load('mean.npy')
     sample = Image.open(sample_path).convert('RGB')
     w = min(sample.size[0], sample.size[1])
